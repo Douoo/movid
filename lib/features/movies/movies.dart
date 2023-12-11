349b21d5b3f0444885bf47ac8813987e24016b4d
Douoo <biruksewberiso@gmail.com>
2023-12-11 16:13:03 +0300

    Refactoring

---
+++ lib/features/movies/movies.dart
@@ -0,0 +1,15 @@
+library movies;
+
+//Pages
+export 'presentation/pages/main_movie_page.dart';
+export 'presentation/pages/movie_detail_page.dart';
+export 'presentation/pages/popular_movies_page.dart';
+export 'presentation/pages/top_rated_movies_page.dart';
+
+//Provider
+export 'presentation/provider/movie_detail_provider.dart';
+export 'presentation/provider/movie_images_provider.dart';
+export 'presentation/provider/movie_list_provider.dart';
+export 'presentation/provider/popular_movies_provider.dart';
+export 'presentation/provider/top_rated_movies_provider.dart';
+export 'presentation/provider/watchlist_movies_provider.dart';
